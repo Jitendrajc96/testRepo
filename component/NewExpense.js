2b53a4ddc9d26c290894b0a33da028a4586a66f3
Jitendra Chauhan <113715765+Jitendrajc96@users.noreply.github.com>
2022-11-09 12:03:31 +0530

    Create NewExpense.js

---
+++ component/NewExpense.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import NewExpense from './NewExpense'
+import './NewExpense.css';
+
+const NewExpense = () => {
+    return <div className='new-expense'>
+        <ExpenseForm />
+    </div>
+}
+
+export default NewExpense;
